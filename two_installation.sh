f831075f0971a2a93c85fcbbd42e595042649f47
SomeshRao007 <111784343+SomeshRao007@users.noreply.github.com>
2023-09-01 17:04:50 +0530

    two_installation.sh

--- two_installation.sh
+++ two_installation.sh
@@ -46,3 +46,8 @@ minikube start --network-plugin=cni --cni=calico
 minikube status
 
 kubectl config view
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
